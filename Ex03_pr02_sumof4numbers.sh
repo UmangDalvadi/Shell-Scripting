dcc0bcbe642ea524483d754d37a456de958d818c
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-29 17:10:06 +0530

    add

---
+++ Ex03_pr02_sumof4numbers.sh
@@ -0,0 +1,16 @@
+#!zsh
+
+echo "Enter 1st number : "
+read a
+
+echo "Enter 2nd number : "
+read b
+
+echo "Enter 3rd number : "
+read c
+
+echo "Enter 4th number : "
+read d
+
+echo "Sum of all numbers : " $(($a+$b+$c+$d))
+

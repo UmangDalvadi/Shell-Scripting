efabb8bd7c3e8f5d21e28ce51dee61d87395b40a
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-07 03:36:39 +0530

    add

--- Experiments/Ex03_pr06_fibonacci_iterative.sh
+++ Experiments/Ex03_pr06_fibonacci_iterative.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
 
+echo -n "Enter the number for searies : "
+read num
+
+a=0
+b=1
+
+for ((i=0; i<=$num; i++))
+do
+    echo -n "$a "
+    temp=$((a + b))
+    a=$b
+    b=$temp
+done

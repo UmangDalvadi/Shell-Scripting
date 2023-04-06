e9f13ee68022a84cc12944969b70e97dbfedd30a
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-07 03:44:54 +0530

    add

---
+++ Experiments/Ex03_pr07_menudrive.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "a. Display calendar of current month 
+b. Display today’s date and time 
+c. Display usernames those are currently logged in the system 
+d. Display your name at given x, y position 
+e. Display your terminal number"
+
+echo -n "Enter character to perform task : "
+read char
+
+case $char in 
+a) echo ;;
+
+
+
+esac

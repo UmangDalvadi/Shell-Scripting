2a0c58bc9e2013af8d68fa22130f5b0565695081
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-17 15:13:07 +0530

    add

---
+++ Experiments/Ex02_Commands.sh
@@ -0,0 +1,23 @@
+#12 AIM : Write a command to display a message hello I am learning operating  system on the terminal
+echo hello I am learning operating  system
+
+#13 AIM : Write a command to display comment and current time on terminal  window using echo command.
+
+#14 AIM : Write a command to create a directory named demo in the default  directory of current user on terminal window.
+
+#15 AIM : Write a command to create a file named GECBVN and display the  contents of it.
+
+#16 AIM : Write a command to concatenate file demo1 and demo2 to third file  demo3.
+
+#17 AIM : Write a command to append data "India" to file aaa.
+
+#18 AIM : List the file whose name starts with a and ends with a.
+
+#19 AIM : Display the number of lines, words and characters in a file named  "gecbvn".
+
+#20 AIM : Change the file permissions of file bbb and change the directory  permissions of directory demo1.
+
+#21 AIM : Display the data in the file in descending order.
+
+#22 AIM : Display the data of file in capital (uppercase only).
+

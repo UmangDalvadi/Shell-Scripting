ff3a48b9f4c76ae327d58a5420b78c02731d9a56
UmangDalvadi <umangdalvadi8@gmail.com>
2023-04-29 19:18:43 +0530

    add

---
+++ Experiments/Ex03_pr08_descendingorder.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+function sort()
+{
+    #  echo ${arr[@]}
+    # len=${#arr[@]}
+    # for i in $(seq $((${#arr[@]}-1)))
+    for i in $(seq ${#arr[@]})
+    do
+        # for (( j=0;j<$((${#arr[@]}-1-$i)); j++))
+        for (( j=0;j<$((${#arr[@]}-$i)); j++))
+        do
+            # echo -n "${arr[$i]} "
+            if [[ ${arr[j]} < ${arr[$((j+1))]} ]]
+            then
+                temp=${arr[$j]}
+                arr[$j]=${arr[$(($j+1))]}
+                arr[$(($j+1))]=$temp
+            fi
+
+        done
+
+    done
+
+    echo -n "${arr[@]} "
+
+}
+
+echo -n "Enter the length of an array : "
+read n;
+
+for i in $(seq $n)
+do
+     read arr[$i]
+done
+
+# echo ${arr[@]}
+
+sort "${arr[@]}"
